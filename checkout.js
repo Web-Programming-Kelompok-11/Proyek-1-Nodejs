c0ae3c9f402835600417f60ce8a084cd0ba51f40
Eugene V. Arends <eugene.535210082@stu.untar.ac.id>
2022-10-04 12:29:01 +0700

    add: checkout.html eventID, sessionID

--- checkout.js
+++ checkout.js
@@ -11,16 +11,30 @@ function findGetParameter(parameterName) { // Gets the GET variables
     return result;
 }
 
-if(findGetParameter("item")) { // Check if parameter matches
-   displayTheBox(findGetParameter("item"));
+if(findGetParameter("eventID")) { // Check if parameter matches
+   displayTheBox(findGetParameter("eventID"));
 }
 var prevN = 0;
 
 function displayTheBox(n) { // Display the selected ticket/item
     var products = document.getElementsByClassName("displayBlock");
-
     products[prevN].className = products[prevN].className.replace(" show", "");
     products[n].className += " show";
 
     prevN = n;
 }
+
+
+var preN = 0;
+
+if(findGetParameter("sessionID")) { // Check if parameter matches
+    displayTheDate(findGetParameter("sessionID"));
+ }
+ 
+ function displayTheDate(n) { // Display the selected ticket/item
+     var products = document.getElementsByClassName("displayDate");
+     products[preN].className = products[preN].className.replace(" show", "");
+     products[n].className += " show";
+
+     preN = n;
+ }
